68541c5ebc9e261bd46fe9159233c169035537fd
Aqib Yaqoob <formanite.yaqoob92@gmail.com>
2018-12-27 03:18:30 +0500

    Added JWT Auth in application

--- rentalapp/database/migrations/2014_10_12_000000_create_users_table.php
+++ rentalapp/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('user_type')->comment('1 => Land Loard, 2 => Tenant');
             $table->rememberToken();
             $table->timestamps();
         });
