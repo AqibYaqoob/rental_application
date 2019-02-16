834f15b61f1dfcc1a192b6177e744d20eb8293e5
Aqib Yaqoob <formanite.yaqoob92@gmail.com>
2019-02-17 01:57:44 +0500

    Added Transaction Modal and Table

---
+++ rentalapp/app/TransactionDetail.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
+use \OwenIt\Auditing\Auditable;
+
+class TransactionDetail extends Model implements AuditableContract
+{
+    use Auditable;
+    protected $table      = 'transaction_details';
+    protected $primaryKey = 'id';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'transaction_type', 'amount', 'payment_option', 'user_id',
+    ];
+}
