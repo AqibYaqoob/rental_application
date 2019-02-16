e0074211aa4e698e0e0fa348985aa5333d85cf54
Aqib Yaqoob <formanite.yaqoob92@gmail.com>
2019-02-16 23:59:28 +0500

    Fixed Bug

--- rentalapp/app/User.php
+++ rentalapp/app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable implements JWTSubject
      */
     protected $table    = 'users';
     protected $fillable = [
-        'TenantId', 'Username', 'password', 'LastPasswordUpdate', 'IsAdmin', 'LastUpdateBy', 'LastUpdateIPAddress', 'CreatedBy', 'CreatedIPAddress', 'LastLogin', 'LastLoginIPAddress', 'email', 'Roles', 'AccountStatus', 'account_type', 'interest_rate', 'name', 'otp_code', 'otp_check', 'user_type', 'device_id',
+        'TenantId', 'Username', 'password', 'LastPasswordUpdate', 'IsAdmin', 'LastUpdateBy', 'LastUpdateIPAddress', 'CreatedBy', 'CreatedIPAddress', 'LastLogin', 'LastLoginIPAddress', 'email', 'Roles', 'AccountStatus', 'account_type', 'interest_rate', 'name', 'otp_code', 'otp_check', 'user_type', 'device_token',
     ];
 
     // Rest omitted for brevity
