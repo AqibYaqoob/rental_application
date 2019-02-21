3fa337d445f1e71e86ad2a31ab6fe1656d094ed2
Aqib Yaqoob <formanite.yaqoob92@gmail.com>
2019-02-21 12:30:42 +0500

    Added User staff access script into Staff Movement

---
+++ rentalapp/app/ChatMessages.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
+use \OwenIt\Auditing\Auditable;
+
+class ChatMessages extends Model implements AuditableContract
+{
+    use Auditable;
+    protected $primaryKey = 'id';
+    protected $table      = 'chat_messages';
+    protected $fillable   = ['identifier', 'message', 'from_user_id'];
+}
